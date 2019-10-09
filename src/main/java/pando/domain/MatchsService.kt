34be6ab38fa90fb01c5cb9bc73f78dd291697bac
Pandogrammer <pandogrammer@gmail.com>
2019-10-09 08:30:55 -0300

    Generacion random de criaturas contra la IA

--- src/main/java/pando/domain/MatchsService.kt
+++ src/main/java/pando/domain/MatchsService.kt
@@ -2,24 +2,35 @@ package pando.domain
 
 import io.reactivex.subjects.PublishSubject
 import pando.creatures.Position
-import pando.creatures.cards.CreatureCard
-import pando.creatures.cards.FluffyCard
-import pando.creatures.cards.SkeletonCard
+import pando.creatures.cards.*
+import kotlin.random.Random
 
 class MatchsService(private val matchs: Matchs) {
     val matchCreated = PublishSubject.create<MatchCreation>()
 
-    private val iaCreatures = {
-        mapOf(Position(1, 1) to SkeletonCard(),
-                Position(2, 1) to FluffyCard())
-    }
-
     fun create(playerCreatures: Map<Position, CreatureCard>, accountId: String): Int {
-        val match = Match(playerCreatures, iaCreatures())
+        val match = Match(playerCreatures, generateIaCreatures())
         val matchId = matchs.add(match)
         matchCreated.onNext(MatchCreation(matchId, accountId))
         match.start()
         return matchId
+    }
+
+    private fun generateIaCreatures(): Map<Position, CreatureCard> {
+        return mapOf(Position(1, 1) to getRandomCreature(),
+                Position(2, 1) to getRandomCreature(),
+                Position(1, 2) to getRandomCreature(),
+                Position(2, 2) to getRandomCreature())
+    }
+
+    private fun getRandomCreature(): CreatureCard {
+        return when (Random.nextInt(1, 4)) {
+            1 -> EyeCard()
+            2 -> SkeletonCard()
+            3 -> FluffyCard()
+            4 -> FrogCard()
+            else -> EyeCard()
+        }
     }
 
     fun get(matchId: Int): Match? {
